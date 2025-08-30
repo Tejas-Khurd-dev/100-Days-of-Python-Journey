f153ee4335c9f25cd4da426a95ee9ddec89603a6
Tejas Khurd <tejaskhurd.dev@gmail.com>
2025-08-30 10:41:58 +0530

    Add Brand_Name_Generator.py

---
+++ Day 1/Brand_Name_Generator.py
@@ -0,0 +1,7 @@
+print("*** Welcome to the Brand Name Generator ***")
+
+city_name = input("Please enter the name of the city where you grew up: ")
+print("Wow! So you grew up in " + city_name + ".")
+
+suffix = input("Please enter your suffix (eg: \"Warriors\", \"Titans\", \"Legends\"):  ")
+print("Your brand name could be " + city_name + " " + suffix)
