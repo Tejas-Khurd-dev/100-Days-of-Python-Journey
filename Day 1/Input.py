ef4ec5758b0185046bbcd967f1b59d8406db03cf
Tejas Khurd <tejaskhurd.dev@gmail.com>
2025-08-30 10:37:27 +0530

    Add Input.py in Day 1

---
+++ Day 1/Input.py
@@ -0,0 +1,3 @@
+# Python sees the input() function inside the print() call.
+# input("What is your name?\n") runs first, because Python must know what to insert into the string.
+print("Hello " + input("What is your name?\n") + "! " +":)")
