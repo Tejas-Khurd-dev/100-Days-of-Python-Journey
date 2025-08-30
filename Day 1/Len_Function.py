6256cab84a3104ba11ee0c24aff81d86a4de52a7
Tejas Khurd <tejaskhurd.dev@gmail.com>
2025-08-30 10:39:22 +0530

    Add Len_Function.py

---
+++ Day 1/Len_Function.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+print("Hello " + name + "!")
+
+print("The length of your input is: ")
+length = len(name)
+print(length)
+
+name = "jack"
+print("Now your name is: " + "Jack and now your name of length is --> ", len(name))
+
+# By default, print() always ends with a newline, so the output number (5) shows up on the next line.
+print(len(input("Enter your name: ")))
