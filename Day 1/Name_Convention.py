6f4a5717a37f0eaf8b3b0d6ee51465017830652e
Tejas Khurd <tejaskhurd.dev@gmail.com>
2025-08-30 10:40:47 +0530

    Add Name_Convention.py

---
+++ Day 1/Name_Convention.py
@@ -0,0 +1,7 @@
+user_name1 = "Angela"
+print(user_name1)
+# "1name" ia not allowed s it can't start with number
+# "name "1  is not allowed as it contain space
+# "name_player1"" is allowed as underscore and number at end or in the middle of variable name is allowed
+# "name&player1" is not allowed as it contain special character
+# "Name" and "name" are two different variables as they are case-sensitive
