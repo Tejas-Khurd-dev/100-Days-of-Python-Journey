a8387a406b281f65a5adf172b53caa32cb77e6ad
Tejas Khurd <tejaskhurd.dev@gmail.com>
2025-08-30 10:36:30 +0530

    Add Intro.py in Day 1 folder

---
+++ Day 1/Intro.py
@@ -0,0 +1,13 @@
+# Write your code below this line 👇
+print("Hello world!\nMyself Tejas Khurd")
+print("Hello" + " " + "Tejas")
+
+# Single line string
+print('Hello')
+
+# Single line string
+print("Hello")
+
+# Multi line string
+print('''Hi! 
+Hello''')
