7292c26b54912ef0f4fb2e9a24cfa51c18516539
Tejas Khurd <tejaskhurd.dev@gmail.com>
2025-08-31 11:31:13 +0530

    Add data_types.py

---
+++ Day 2/data_types.py
@@ -0,0 +1,21 @@
+# Pulling out character from the string is called subscripting
+print("subscripting"[0])
+print("subscripting"[4])
+
+print("subscripting"[-1])
+print("subscripting"[-6])
+
+print("132" + "345") # Concatenation
+
+# Integer = Whole Numbers
+print(123 + 345)
+
+# Large Integers --> Commas are replaced by (_)
+print(10_000_000)
+
+# Float = Floating Point Numbers
+print(123.56)
+
+# Boolean
+print(True)
+print(False)
