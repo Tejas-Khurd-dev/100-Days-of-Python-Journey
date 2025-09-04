56faa5ee443cfd8001188c74aa45e37ae61fd81b
Tejas Khurd <tejaskhurd.dev@gmail.com>
2025-09-05 03:26:47 +0530

    Update hurle_1_using_while_loop.py

--- Day 6/Reeborg's_World/hurle_1_using_while_loop.py
+++ Day 6/Reeborg's_World/hurle_1_using_while_loop.py
@@ -1,29 +1,25 @@
-def turn_around():
-    turn_left()
-    turn_left()
+# def turn_around():
+#     turn_left()
+#     turn_left()
     
-def turn_right():   
-    turn_left()
-    turn_left()
-    turn_left()
+# def turn_right():   
+#     turn_left()
+#     turn_left()
+#     turn_left()
 
-def jump():
-    move()
-    turn_left()
-    move()
-    turn_right()
-    move()
-    turn_right ()
-    move()
-    turn_left()
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right ()
+#     move()
+#     turn_left()
     
-number_of_hurdles = 6
+# number_of_hurdles = 6
 
-while number_of_hurdles > 0:
-    jump()
-    number_of_hurdles -= 1 
+# while number_of_hurdles > 0:
+#     jump()
+#     number_of_hurdles -= 1 
 
-################################################################
-# WARNING: Do not change this comment.
-# Library Code is below.
-################################################################
