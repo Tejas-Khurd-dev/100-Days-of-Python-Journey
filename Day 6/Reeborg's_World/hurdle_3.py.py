c71423bd66f249d3fa911e48130264b58c007dd8
Tejas Khurd <tejaskhurd.dev@gmail.com>
2025-09-05 03:26:07 +0530

    Update hurdle_3.py.py

--- Day 6/Reeborg's_World/hurdle_3.py.py
+++ Day 6/Reeborg's_World/hurdle_3.py.py
@@ -1,26 +1,26 @@
-def turn_around():
-    turn_left()
-    turn_left()
+# def turn_around():
+#     turn_left()
+#     turn_left()
     
-def turn_right():   
-    turn_left()
-    turn_left()
-    turn_left()
+# def turn_right():   
+#     turn_left()
+#     turn_left()
+#     turn_left()
 
-def jump():
-    turn_left()
-    move()
-    turn_right()
-    move()
-    turn_right ()
-    move()
-    turn_left()
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right ()
+#     move()
+#     turn_left()
 
-while not at_goal():
-    if front_is_clear():
-       move()
-    else:
-        jump()
+# while not at_goal():
+#     if front_is_clear():
+#        move()
+#     else:
+#         jump()
         
     #OR   
     
@@ -29,8 +29,3 @@ while not at_goal():
     #else:
         #move()
 
-
-################################################################
-# WARNING: Do not change this comment.
-# Library Code is below.
-################################################################
