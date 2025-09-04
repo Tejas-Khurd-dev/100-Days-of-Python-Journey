bc13e49c9a474b5be38011b43540c81159216488
Tejas Khurd <tejaskhurd.dev@gmail.com>
2025-09-05 03:25:18 +0530

    Update hurdle_1_using_for_loop.py

--- Day 6/Reeborg's_World/hurdle_1_using_for_loop.py
+++ Day 6/Reeborg's_World/hurdle_1_using_for_loop.py
@@ -1,26 +1,21 @@
-def turn_around():
-    turn_left()
-    turn_left()
+# def turn_around():
+#     turn_left()
+#     turn_left()
     
-def turn_right():   
-    turn_left()
-    turn_left()
-    turn_left()
+# def turn_right():   
+#     turn_left()
+#     turn_left()
+#     turn_left()
 
-def jump():
-    move()
-    turn_left()
-    move()
-    turn_right()
-    move()
-    turn_right ()
-    move()
-    turn_left()
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right ()
+#     move()
+#     turn_left()
     
-for step in range(6):
-    jump()
-
-################################################################
-# WARNING: Do not change this comment.
-# Library Code is below.
-################################################################
+# for step in range(6):
+#     jump()
