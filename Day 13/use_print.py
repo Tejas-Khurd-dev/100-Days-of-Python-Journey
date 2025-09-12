4acd907fdabb1ee2fdcee385cd1632fd5ea032f0
Tejas Khurd <tejaskhurd.dev@gmail.com>
2025-09-12 22:17:09 +0530

    Add use_print.py

---
+++ Day 13/use_print.py
@@ -0,0 +1,14 @@
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+
+# print(f"Pagers = {pages}")
+# print(f"Word per page = {word_per_page}")
+print(total_words)
