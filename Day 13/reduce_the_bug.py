6fcf15dc049c0708a5fbe2d3428a0b4d3e434325
Tejas Khurd <tejaskhurd.dev@gmail.com>
2025-09-12 22:16:00 +0530

    Add reduce_the_bug.py

---
+++ Day 13/reduce_the_bug.py
@@ -0,0 +1,11 @@
+from random import randint
+
+# dice_images = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(1, 6)
+# dice_num = 1
+# dice_num = 3
+# print(dice_images[dice_num])
+
+dice_images = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5)
+print(dice_images[dice_num])
