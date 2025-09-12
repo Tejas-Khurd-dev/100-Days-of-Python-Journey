945b0d7ed53c5bdc6c94585c2de255f0bd44e958
Tejas Khurd <tejaskhurd.dev@gmail.com>
2025-09-12 22:13:13 +0530

    Add describe_the_problem.py

---
+++ Day 13/describe_the_problem.py
@@ -0,0 +1,23 @@
+# def my_function():
+#     for i in range(1, 20):
+#         if i == 20:
+#             print("You got it")
+#
+#
+# my_function()
+
+# Describe the Problem - Write your answers as comments:
+# 1. What is the for loop doing?
+# Printing 1 to 19
+# 2. When is the function meant to print "You got it"?
+# if i == 20
+# 3. What are your assumptions about the value of i?
+# 1 <= i < 20
+
+def my_function():
+    for i in range(1, 21):
+        if i == 20:
+            print("You got it")
+
+
+my_function()
