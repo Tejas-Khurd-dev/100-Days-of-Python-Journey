8a4ab523d497e9c5e7dc9f88a5b5a3ad8632c363
Tejas Khurd <tejaskhurd.dev@gmail.com>
2025-09-15 23:29:32 +0530

    Add turtle_module.py

---
+++ Day 16/turtle_module.py
@@ -0,0 +1,12 @@
+import another_module
+print(another_module.another_variable)
+
+import turtle
+timmy = turtle.Turtle()
+timmy.shape("turtle")
+timmy.color("orange", "green")
+timmy.forward(200)
+
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
